22f2cd50a2f9eae9e35e8d53b07bd744399f53b3
김태경 <seaking7@gmail.com>
2023-06-02 20:48:44 +0900

    indicator 기능 적용

--- imageSlider/src/js/imageSlider.js
+++ imageSlider/src/js/imageSlider.js
@@ -13,12 +13,16 @@ export default class ImageSlider {
 
   previousBtnEl;
 
+  indicatorWrapEl;
+
   constructor() {
     this.assignElement();
     this.initSliderNumber();
     this.initSliderWidth();
     this.initSliderListWidth();
     this.addEvent();
+    this.createIndicator();
+    this.setIndicator();
   }
 
   assignElement() {
@@ -26,6 +30,7 @@ export default class ImageSlider {
     this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
     this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
     this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+    this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
   }
 
   initSliderNumber() {
@@ -45,6 +50,21 @@ export default class ImageSlider {
   addEvent() {
     this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
     this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    this.indicatorWrapEl.addEventListener(
+      'click',
+      this.onClickIndicator.bind(this),
+    );
+  }
+
+  onClickIndicator(event) {
+    const indexPosition = parseInt(event.target.dataset.index, 10);
+    if (Number.isInteger(indexPosition)) {
+      this.#currentPosition = indexPosition;
+      this.sliderListEl.style.left = `-${
+        this.#sliderWidth * this.#currentPosition
+      }px`;
+    }
+    this.setIndicator();
   }
 
   moveToRight() {
@@ -55,6 +75,7 @@ export default class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#sliderWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
   }
 
   moveToLeft() {
@@ -65,5 +86,23 @@ export default class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#sliderWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
+  }
+
+  createIndicator() {
+    const docFragment = document.createDocumentFragment();
+    for (let i = 0; i < this.#sliderNumber; i += 1) {
+      const li = document.createElement('li');
+      li.dataset.index = i;
+      docFragment.appendChild(li);
+    }
+    this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+  }
+
+  setIndicator() {
+    this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+    this.indicatorWrapEl
+      .querySelector(`ul li:nth-child(${this.#currentPosition + 1})`)
+      .classList.add('active');
   }
 }
