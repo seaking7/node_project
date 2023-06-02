f57eeba9eadae7c0f96316bdec5cc3ce1763958f
김태경 <seaking7@gmail.com>
2023-06-02 15:19:58 +0900

    key 입력 이벤트 적용 및 한글입력시 오류처리

--- keyboard/src/js/keyboard.js
+++ keyboard/src/js/keyboard.js
@@ -2,7 +2,8 @@ export class Keyboard {
   #swichEl;
   #fontSelectEl;
   #containerEl;
-
+  #keyboardEl;
+  #inputGroupEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -13,11 +14,32 @@ export class Keyboard {
     // this.#swichEl = document.getElementById("switch");
     this.#swichEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
   }
 
   #addEvent() {
     this.#swichEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", (event) => {
+      console.log(event.code);
+      console.log(event.key, /[ㄱ-ㅎ|ㅏ-ㅣ|가=힣]/.test(event.key));
+      this.#inputGroupEl.classList.toggle(
+        "error",
+        /[ㄱ-ㅎ|ㅏ-ㅣ|가=힣]/.test(event.key)
+      );
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.add("active");
+    });
+    document.addEventListener("keyup", (event) => {
+      console.log(event.code);
+      if (this.#keyboardEl.querySelector(`[data-code=${event.code}]`)) {
+        this.#keyboardEl
+          .querySelector(`[data-code=${event.code}]`)
+          .classList.remove("active");
+      }
+    });
   }
 
   #onChangeTheme(event) {
