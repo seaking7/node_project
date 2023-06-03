85f51683287cd92745abc6105315ae853db8baa7
김태경 <seaking7@gmail.com>
2023-06-03 14:13:30 +0900

    달력 스타일 적용

--- datePicker/src/js/index.js
+++ datePicker/src/js/index.js
@@ -1,5 +1,84 @@
-console.log("hi")
+class DatePicker{
+    monthData = [
+        'January',
+         'February',
+         'March',
+         'April',
+         'May',
+         'June', 
+         'July',
+         'August', 
+         'September', 
+         'October', 
+         'November', 
+         'December',
+    ];
 
-let a=1
-let b=22
-console.log(a+b)
+    #calenderDate = {
+        data: '',
+        date: 0,
+        month: 0,
+        year: 0,
+    };
+
+    selectedDate = {
+        data: '',
+        date: 0,
+        month: 0,
+        year: 0,
+    };
+
+    datePickerEl;
+    dateInputEl;
+    calenderEl;
+    calenderMonthEl;
+    monthContentEl;
+    nextBtnEl;
+    prevBtnEl;
+    calenderDatesEl;
+
+    constructor(){
+
+        this.assignElement();
+        this.initCalenderDate();
+        this.addEvent();
+    }
+
+    initCalenderDate(){
+        const data = new Date();
+        const date = data.getDate();
+        const month = data.getMonth();
+        const year = data.getFullYear();
+        this.#calenderDate = {
+            data,
+            date,
+            month,
+            year,
+        };
+        console.log(this.#calenderDate);
+    }
+
+    assignElement(){
+        this.datePickerEl = document.getElementById("date-picker");
+        this.dateInputEl = this.datePickerEl.querySelector('#date-input')
+        this.calenderEl = this.datePickerEl.querySelector('#calendar');
+        this.calenderMonthEl = this.calenderEl.querySelector('#month');
+        this.monthContentEl = this.calenderMonthEl.querySelector('#content');
+        this.nextBtnEl = this.calenderMonthEl.querySelector('#next');
+        this.prevBtnEl = this.calenderMonthEl.querySelector('#prev');
+
+        this.calenderDatesEl = this.calenderEl.querySelector('#dates');
+
+
+    }
+
+    addEvent() {
+        this.dateInputEl.addEventListener("click", this.toggleCalender.bind(this));
+    }
+
+    toggleCalender() {
+        this.calenderEl.classList.toggle('active');
+    }
+}
+
+const datePicker = new DatePicker;
